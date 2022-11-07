f45243d4d713e6b1b9afbed6021c2045f78c6576
JunxiC <chenjon09@gmail.com>
2022-11-07 10:16:46 -0500

    DOM COMMIT 4

--- index.js
+++ index.js
@@ -4,21 +4,13 @@ const DOMSelectors = {
   input: document.querySelector(`#input`),
 };
 
-DOMSelectors.box.insertAdjacentHTML(
-  "beforebegin",
-  `We can add text into an HTML file by writing it in JS!`
-);
-// insertAdjacentHTML will allow you to add HTML elements into your project through the JS. The positioning of this is relative to other DOM elements, and can be positioned around them. This positioning can be done with "beforebegin", "afterbegin", "beforeend", and "afterend".
-
 DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value; // .value lets you get the values of an element, in this case something you typed in
-
-  console.log(input);
-
+  let input = DOMSelectors.input.value;
   DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
-    `
-          <p class = 'extra-text'>${input}</p>
-      `
+    `<p>Put HTML Here 
+    <button type="button" id="remove-btn">Remove</button>
+    ${input}</p>`
   );
-}); // this function will cause new HTML to be added when the input button is clicked.
+  DOMSelectors.input.value = "";
+});
