4f81c8b416a4e144d82b8c8994782ab306f8bf5a
JunxiC <chenjon09@gmail.com>
2022-11-21 10:16:55 -0500

    DOM COMMIT 13

--- index.js
+++ index.js
@@ -15,14 +15,21 @@ DOMSelectors.button.addEventListener("click", function () {
   let address = DOMSelectors.address.value;
   DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
-    `<p id=idremove><span>My name is ${name}. I am ${age} years old. My social security number is ${social}. I live at ${address}.
+    `<p id="idremove"><span>My name is ${name}. I am ${age} years old. My social security number is ${social}. I live at ${address}.
     <button type=button id="removebutton" class="remove">Remove</button></span>
     </p>`
   );
-  DOMSelectors.name.value = "";
-  DOMSelectors.age.value = "";
-  DOMSelectors.social.value = "";
-  DOMSelectors.address.value = "";
+
+  let buttonremove = document.getElementById("removebutton");
+  buttonremove.addEventListener("click", function removeCard(event) {
+    const element = document.getElementById("idremove");
+    element.remove();
+
+    DOMSelectors.name.value = "";
+    DOMSelectors.age.value = "";
+    DOMSelectors.social.value = "";
+    DOMSelectors.address.value = "";
+  });
 });
 
 function green(background) {
@@ -56,20 +63,30 @@ DOMSelectors.button.addEventListener("click", function () {
 //add event listener
 //event.target what did I click?
 
-// const matches = document.querySelectorAll("remove");
+// const matches = document.querySelectorAll("idremove");
+
+// const values = [
+//   {
+//     name: document.querySelector("name"),
+//     age: "",
+//     social: "",
+//     address: "",
+//   },
+// ];
+// console.log(values);
 
 // console.log(matches);
 
-const removelist = document.createElement("idbutton");
-const removespan = document.createElement("span");
-const removebutton = document.createElement("removebutton");
+// const removelist = document.getElementById("idbutton");
+// const removespan = document.createElement("span");
+// const removebutton = document.createElement("removebutton");
 
-removebutton.textContent = "Remove";
+// removebutton.textContent = "Remove";
 
-removebutton.addEventListener("click", () => {
-  list.removeChild(removelist);
-});
+// removebutton.addEventListener("click", () => {
+//   list.removeChild(removelist);
+// });
 
-console.log(removelist);
-console.log(removespan);
-console.log(removebutton);
+// console.log(removelist);
+// console.log(removespan);
+// console.log(removebutton);
